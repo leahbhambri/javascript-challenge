d928daada8c82164489ae8d3ed281d2599905ea2
leahbhambri <leahbhambri@yahoo.com>
2021-03-24 21:54:01 -0500

    updated level 2 and removed form issue

--- UFO-level-2/static/app.js
+++ UFO-level-2/static/app.js
@@ -7,10 +7,10 @@ var tbody = d3.select("tbody")
 
 //print each data element
 data.forEach(function(UFOsiting) {
-    console.log(UFOsiting)
+    // console.log(UFOsiting)
     var row = tbody.append("tr")
     Object.entries(UFOsiting).forEach(function([key, value]) {
-        console.log(key, value);
+        // console.log(key, value);
         var dataElement = row.append("td")
         dataElement.text(value)
       });
@@ -24,23 +24,26 @@ var enter = d3.select(".form-control")
 function filterList() {
   var inputElement = d3.select(".form-control")
   var inputValue = inputElement.property("value")
-  //clear table body
-  tbody.text("")
+  
   //select table and body elements
   var table = d3.select(".table")
   var tbody = d3.select("tbody")
+  
+  //clear table body
+  tbody.text("")
 
-  //NEED TO CONVERT DATE OF INPUT TO DATETIME??
-  filteredData = data.filter(d => d.datetime ===inputValue)
+  console.log(inputValue)
+  
+  //filter data for date entered
+  filteredData = data.filter(d => d.datetime === inputValue)
   //populate table with filtered list data
   filteredData.forEach(function(UFOsiting) {
     
       // Prevent the page from refreshing
-​    d3.event.preventDefault();
-    console.log(UFOsiting)
+    // console.log(UFOsiting)
     var row = tbody.append("tr")
     Object.entries(UFOsiting).forEach(function([key, value]) {
-        console.log(key, value);
+        // console.log(key, value);
         var dataElement = row.append("td")
         dataElement.text(value)
       });
